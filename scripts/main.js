d2da7ed769098a1ebca6ce0ed31317b4e8b0d0d1
danilaziz <danielazizz52@gmail.com>
2024-02-14 12:53:06 +0800

    mengubah tampilan keseluruhan

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myNav");
+var btns = header.getElementsByClassName("btns");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
